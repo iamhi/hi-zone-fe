2490d1b193585508e5f92992fa504ab4f4cf79b3
iamhi <inpactt@gmail.com>
2022-01-22 20:10:57 +0100

    SpotifyComponent initial

--- src/components/SpotifyComponent/SpotifyComponent.jsx
+++ src/components/SpotifyComponent/SpotifyComponent.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+// TODO: Fetch the playlist url based on the mood
 const SpotifyComponent = () => (
 	<div className="spotify-component">
-		<p>Spotify player</p>
-		<p>Mood relate music</p>
+		<iframe
+			className="spotify-component__spotify-iframe"
+			title="spotify-player"
+			src="https://open.spotify.com/embed/playlist/2V3LZ7Pjk6HNhlikDn4sl2?utm_source=generator"
+			width="100%"
+			height="380"
+			frameBorder="0"
+			allowFullScreen=""
+			allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
 	</div>
 );
 
--- src/components/TitleComponent/TitleComponent.jsx
+++ src/components/TitleComponent/TitleComponent.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const TitleComponent = () => (
 	<div className="title-component">
-		<i className="fab fa-twitter"></i>
 		<div className="title-component__title">Hi-ZONE</div>
 		<div className="title-component__logo">👋👋👋</div>
 	</div>
