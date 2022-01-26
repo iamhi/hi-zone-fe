096d2503d62c145b36866f5360c694c1163cfded
iamhi <inpactt@gmail.com>
2022-01-26 21:29:38 +0100

    SpotifyComponent mood urls

--- src/components/SpotifyComponent/SpotifyComponent.jsx
+++ src/components/SpotifyComponent/SpotifyComponent.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-// TODO: Fetch the playlist url based on the mood
-const SpotifyComponent = () => (
-	<div className="spotify-component">
-		<iframe
-			className="spotify-component__spotify-iframe"
-			title="spotify-player"
-			src="https://open.spotify.com/embed/playlist/2V3LZ7Pjk6HNhlikDn4sl2?utm_source=generator"
-			width="100%"
-			height="380"
-			frameBorder="0"
-			allowFullScreen=""
-			allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
-	</div>
-);
+import spotifyContent from '@assets/content/spotify-content.json';
+import { selectCurrentMood } from '@redux/slices/userPreferenceSlice';
+
+const SpotifyComponent = () => {
+	const mood = useSelector(selectCurrentMood);
+	const { url: currentMoodUrl } = spotifyContent.moods[mood];
+
+	return (
+		<div className="spotify-component">
+			<iframe
+				className="spotify-component__spotify-iframe"
+				title="spotify-player"
+				src={currentMoodUrl}
+				width="100%"
+				height="380"
+				frameBorder="0"
+				allowFullScreen=""
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
+		</div>
+	);
+};
 
 export default SpotifyComponent;
