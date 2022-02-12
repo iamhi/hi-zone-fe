e086124ee9866cfb62844effd13b347da93c07e8
iamhi <inpactt@gmail.com>
2022-02-12 17:27:56 +0100

    Small mobile css improvement

--- src/components/NoteApplicationComponent/storage.js
+++ src/components/NoteApplicationComponent/storage.js
@@ -2,11 +2,8 @@ import debounce from 'lodash.debounce';
 
 const NOTE_APPLICATION_NOTE_KEY = 'note-application__note-key';
 
-export const getNoteData = () =>
-	localStorage.getItem(NOTE_APPLICATION_NOTE_KEY) || '';
+export const getNoteData = () => localStorage.getItem(NOTE_APPLICATION_NOTE_KEY) || '';
 
-export const saveNoteData = debounce((value) =>
-	localStorage.setItem(NOTE_APPLICATION_NOTE_KEY, value), 500);
+export const saveNoteData = debounce((value) => localStorage.setItem(NOTE_APPLICATION_NOTE_KEY, value), 500);
 
-export const deleteNoteData = () =>
-	localStorage.removeItem(NOTE_APPLICATION_NOTE_KEY);
+export const deleteNoteData = () => localStorage.removeItem(NOTE_APPLICATION_NOTE_KEY);
