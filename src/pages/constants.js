8543f3f0ee34b03ac1420ba91745c5a242b75199
iamhi <inpactt@gmail.com>
2022-06-19 21:12:47 +0200

    paths

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,4 +1,4 @@
 const appPrefix = process.env.APP_PREFIX;
-export const ABOUT_ROUTE = appPrefix + '/';
-export const ZONE_ROUTE = appPrefix + '/zone';
-export const BLOG_ROUTE = appPrefix + '/blog';
+export const ABOUT_ROUTE = process.env.APP_PREFIX + '/';
+export const ZONE_ROUTE = process.env.APP_PREFIX + '/zone';
+export const BLOG_ROUTE = process.env.APP_PREFIX + '/blog';
